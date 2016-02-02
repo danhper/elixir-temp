159268d4121db6a71789d3a9bd2d3692c96dd4e3
Dmitry Russ <dmitry.a.russ@gmail.com>
2016-02-02 18:05:01 +0100

    fix Temp.open! with fun

--- lib/temp.ex
+++ lib/temp.ex
@@ -98,6 +98,7 @@ defmodule Temp do
   def open!(options \\ nil, func \\ nil) do
     case open(options, func) do
       {:ok, res, path} -> {res, path}
+      {:ok, path} -> path
       {:error, err} -> raise Temp.Error, message: err
     end
   end
